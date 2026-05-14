aecdfc2de8a166b8528290191bbdc7056bf1bc0f
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-05-13 18:49:55 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (0.00%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,17 +1,7 @@
-import numpy as np
 class Solution(object):
     def isGood(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        n = len(nums)
-        max_n = max(nums)
-        u = len(np.unique(nums))
-        if (max_n + 1 != n) or (u != max_n):
-            return False
-        count = nums.count(max_n)
-        if count == 2:
-            return True
-        else:
-            return False
+        return sorted(nums)==list(range(1,len(nums)))+[len(nums)-1]
