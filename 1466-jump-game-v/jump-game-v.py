2e4cfd6bb90d1f694701a40a7c7beab6944a98d2
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-05-23 18:27:55 -0700

    Time: 561 ms (40.00%) | Memory: 23.7 MB (20.00%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def maxJumps(self, arr, d):
+        """
+        :type arr: List[int]
+        :type d: int
+        :rtype: int
+        """
+        n = len(arr)
+        memo = {}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+
+            best = 1  # starting at index i counts as visiting 1 index
+
+            # jump to the right
+            for j in range(i + 1, min(n, i + d + 1)):
+                if arr[j] >= arr[i]:
+                    break
+
+                best = max(best, 1 + dfs(j))
+
+            # jump to the left
+            for j in range(i - 1, max(-1, i - d - 1), -1):
+                if arr[j] >= arr[i]:
+                    break
+
+                best = max(best, 1 + dfs(j))
+
+            memo[i] = best
+            return best
+
+        ans = 0
+
+        for i in range(n):
+            ans = max(ans, dfs(i))
+
+        return ans
