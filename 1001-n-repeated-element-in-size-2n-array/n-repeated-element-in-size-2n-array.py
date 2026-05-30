bc12d0191c8735145872ea7e9579b7442d795d26
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-05-30 14:20:15 -0700

    Time: 0 ms (100.00%) | Memory: 13.2 MB (67.64%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,15 +1,12 @@
-from collections import Counter
 class Solution(object):
     def repeatedNTimes(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        data = {}
-        for i in nums:
-            if i in data:
-                data[i] +=1
-            else:
-                data[i] = 1
-        return max(data, key=data.get)
-        
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
