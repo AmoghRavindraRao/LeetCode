bbe6aa8bfcaaac965e70ed590edf5079066e9f0e
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-06-09 17:03:48 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (25.36%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -11,10 +11,7 @@ class Solution(object):
         for i in range(n - 1):
             x = abs(points[i + 1][0] - points[i][0])
             y = abs(points[i + 1][1] - points[i][1])
-            if x >= y:
-                cost += x
-            else:
-                cost += y
+            cost += max(x, y) 
         
         return cost
 
