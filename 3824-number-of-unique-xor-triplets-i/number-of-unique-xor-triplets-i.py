fe7d33173a8f63af7fa2792876bc923c087e680a
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-07-22 17:13:35 -0700

    Time: 0 ms (100.00%) | Memory: 21.5 MB (12.50%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
@@ -6,5 +6,5 @@ class Solution(object):
         """
         n = len(nums)
         if n <= 2: return n
-        bits = n.bit_length() - 1
-        return 1 << (bits+1)
+        bits = n.bit_length()
+        return 1 << bits
