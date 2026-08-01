be21077800c44c9a7780b597b0d55400c97dcccc
Amogh Ravindra Rao <92088400+AmoghRavindraRao@users.noreply.github.com>
2026-07-31 18:53:42 -0700

    Time: 1682 ms (11.81%) | Memory: 19.4 MB (56.14%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,12 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def best(left: int, right: int) -> int:
+            if left == right:
+                return nums[left]
+
+            take_left = nums[left] - best(left + 1, right)
+            take_right = nums[right] - best(left, right - 1)
+
+            return max(take_left, take_right)
+
+        return best(0, len(nums) - 1) >= 0
